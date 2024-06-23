2853e3c2770cc04da74a16081139abf7ce9f964a
Thanh <thanhcap3@gmail.com>
2024-06-23 22:34:16 +0700

    first commit

---
+++ Script/item.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class item : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Console.WriteLine("an apple");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void 
+	
+}
